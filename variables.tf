f6943043bef65d6b3fb04be7faa55e7d509d2fc3
abhisha7 <31888111+abhisha7@users.noreply.github.com>
2019-11-13 15:19:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "az_count" {
 
 variable "app_image" {
   description = "Docker image to run in the ECS cluster"
-  default     = "bradfordhamilton/crystal_blockchain:latest"
+  default     = "abhisha7/nginx"
 }
 
 variable "app_port" {
